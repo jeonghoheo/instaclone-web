7ae42156b8c1b68bcc20afaad6606e96c17024f7
jeonghoheo <heojeongho1992@gmail.com>
2021-05-14 16:04:35 +0900

    add - Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/home";
+import Login from "./screens/login";
+import NotFound from "./screens/not-found";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
