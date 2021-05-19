77d7631d2efb76da46ccc86d8f4305275b49d37d
jeonghoheo <heojeongho1992@gmail.com>
2021-05-19 11:22:10 +0900

    add - Shared Components

---
+++ src/components/auth/button.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+interface IProps {}
+
+const SButton = styled.input`
+  border: none;
+  border-radius: 3px;
+  margin-top: 12px;
+  background-color: ${(props) => props.theme.accent};
+  color: white;
+  text-align: center;
+  padding: 8px 0px;
+  font-weight: 600;
+  width: 100%;
+`;
+
+function Button<P extends IProps>(props: P) {
+  return <SButton {...props} />;
+}
+
+export default Button;
