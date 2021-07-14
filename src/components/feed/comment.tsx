0c552556a8e277efbba4277c02b0528c7ecbc054
jeonghoheo <heojeongho1992@gmail.com>
2021-07-14 21:00:10 +0900

    add - Parsing Hashtags

--- src/components/feed/comment.tsx
+++ src/components/feed/comment.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+import sanitizeHtml from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
@@ -9,13 +11,35 @@ interface IProps {
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }: IProps) {
+  const cleanedPayload = useMemo(
+    () =>
+      sanitizeHtml(
+        payload.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
+        {
+          allowedTags: ["mark"]
+        }
+      ),
+    [payload]
+  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload
+        }}
+      ></CommentCaption>
     </CommentContainer>
   );
 }
